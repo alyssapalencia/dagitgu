a2e5b252af3cf94e1881e9648a067ad3beec8efd
Justin Malana <justinamalana@su.edu.ph>
2018-01-11 00:36:30 +0800

    updated

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -18,11 +18,68 @@ export class RegisterPage {
   pass: any;
   conPass: any;
 
+  dbEmail: any[] = [];
+  dbUser: any[] = [];
+
+  wrongPass = true;
+  dupEmail = true;
+  dupUser = true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseService: ProviderDagitProvider, public alertCtrl: AlertController) {
+    this.userInfo = this.firebaseService.getUserDetail();
+    var i = 0;
+    this.userInfo.subscribe(snapshots => {
+      snapshots.forEach(snapshot => {
+        this.dbEmail[i] = snapshot.val().emailAddress;
+        this.dbUser[i] = snapshot.val().username;
+        i++;
+      });
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  check(){
+    for(var i=0; i<this.dbEmail.length; i++){
+      if(this.email != this.dbEmail[i]){
+        this.dupEmail = false;
+      }
+      else{
+        this.dupEmail = true;
+      }
+    }
+
+    for(var j=0; j<this.dbUser.length; j++){
+      if(this.user != this.dbUser[j]){
+        this.dupUser = false;
+      }
+      else{
+        this.dupUser = true;
+      }
+    }
+
+    if(this.pass == this.conPass){
+      this.wrongPass = false;
+    }
+
+    if(!this.dupEmail){
+      if(!this.dupUser){
+        if(!this.wrongPass){
+          this.addUser();
+        }
+        else{
+          console.log("wrong password");
+        }
+      }
+      else{
+        console.log("username already exists");
+      }
+    }
+    else{
+      console.log("email already used");
+    }
   }
 
   openLogin() {
